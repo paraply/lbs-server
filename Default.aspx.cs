71311c7330f8708fdac5e5c81e05db37cad26f62
Mikael <git@evinja.se>
2016-05-09 14:10:45 +0200

    display ip and uid if uid is provided

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,19 @@ public partial class _Default : System.Web.UI.Page
 {
    protected void Page_Load(object sender, EventArgs e)
    {
+        String uid = Request.QueryString["id"];
+        if (!String.IsNullOrEmpty(uid)) // We only want to store data when we get an ID
+        {
+            Response.Write("GOT ID:" + uid + "</br>");
+            String ip;
+            HttpRequest httpReq = HttpContext.Current.Request;
+            ip = httpReq.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (String.IsNullOrEmpty(ip))
+            {
+                ip = httpReq.ServerVariables["REMOTE_ADDR"];
+            }
+            Response.Write("IP: " + ip);
+        }
 
    }
 }
